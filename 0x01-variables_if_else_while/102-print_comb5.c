0d04d60831a1f2870af5fe4c91caa7ad7488e2b6
Gabriel Wachira <gabbyk.com@gmail.com>
2023-02-19 00:47:57 -0800

    Add 102-print_comb5.c program file

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: The program prints "00 01, " ... "98 99"
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i, j, k, m;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				for (m = 0; m <= 9; m++)
+				{
+					if ((i == k && j == m) || (k < i || m < j))
+					{
+						continue;
+					} else
+					{
+						putchar('0' + i);
+						putchar('0' + j);
+
+						putchar(' ');
+
+						putchar('0' + k);
+						putchar('0' + m);
+					}
+
+					if (i == 9 && j == 8 && k == 9 && m == 9)
+					{
+						putchar('\n');
+					} else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+
+				}
+			}
+		}
+	}
+	return (0);
+}
