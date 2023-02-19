8670fa6ad2073ca741cc9ed3582650593beadf5f
Gabriel Wachira <gabbyk.com@gmail.com>
2023-02-19 04:14:43 -0800

    Add 101-quote.c program file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: The program prints -
+ * 'and that piece of art is useful - Dora Korpar, 2015-10-19,
+ * followed by a new line, to the standard error.
+ *
+ * Return: Always 1
+ */
+
+int main(void)
+{
+	const char *quote = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
+
+	size_t len = strlen(quote);
+
+	write(1, quote, len);
+	write(2, quote, len);
+	return (1);
+}
