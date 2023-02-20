d8236f2d952914f0d50f37b1724e6fa93cb90789
Gabriel Wachira <gabbyk.com@gmail.com>
2023-02-20 11:56:04 -0800

    Modify 101-quote.c program file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -15,11 +15,10 @@
 
 int main(void)
 {
-	const char *quote = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
+	const char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	size_t len = strlen(quote);
 
-	write(1, quote, len);
-	write(2, quote, len);
-	return (1);
+	write(STDERR_FILENO, quote, len);
+	return (EXIT_FAILURE);
 }
