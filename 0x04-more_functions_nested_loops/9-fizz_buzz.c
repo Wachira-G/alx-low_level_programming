b71d77085ca40ff77518d70e8c6d5b293f9deb13
Gabriel Wachira <gabbyk.com@gmail.com>
2023-02-24 12:31:31 -0800

    Modify 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ * Description: prints 0-99 if div by 3 fizz
+ * div by 5 buzz if both fizzbuzz
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
 	int i;
 
-	for (i = 1; i < 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 			printf("FizzBuzz ");
@@ -13,7 +20,7 @@ int main(void)
 		else if (i % 5 == 0)
 			printf("Buzz ");
 		else
-			printf("%d ",i);
+			printf("%d ", i);
 	}
 	printf("\n");
 	return (0);
