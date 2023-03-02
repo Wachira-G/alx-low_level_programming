45b5cd0878030d3d65fe106ffd2e1f11bb6d2cc3
Gabriel Wachira <gabbyk.com@gmail.com>
2023-03-02 20:04:03 +0300

    Modify 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,20 +10,17 @@
 
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+	char lower[] = {'a', 'e', 'o', 'l', 't'};
+	char le[] = {'4', '3', '0', '1', '7'};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == lower[j] || s[i] == (lower[j] - 'a' + 'A'))
+				s[i] = le[j];
+		}
 	}
 	return (s);
 }
