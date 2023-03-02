478994141e71769da7692dc6e3f7ccd1eefe3c38
Gabriel Wachira <gabbyk.com@gmail.com>
2023-03-02 17:40:40 +0300

    Modify 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,12 +12,18 @@
 
 int _strcmp(char *s1, char *s2)
 {
+
 	int i = 0, j;
 
 	do {
 		j = s1[i] - s2[i];
 		i++;
-	} while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0');
-
+	} while (j == 0 && s1[i] != '\0' && s2[i] != '\0');
+/*
+ *	int i, j;
+ *
+ *	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+ *		j = s1[i] - s2[i];
+ */
 	return (j);
 }
