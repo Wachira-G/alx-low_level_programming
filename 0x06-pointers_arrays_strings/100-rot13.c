a2c052f3160780ca1e2df8ecf2a2eaa5a62ec135
Gabriel Wachira <gabbyk.com@gmail.com>
2023-03-03 12:34:51 +0300

    Add 100-rot13.c 100-main.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * Description - rotates each char 13 places along the alphabet
+ * @s: string to encode
+ * Return: pointer to encoded string
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char alpha[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+		'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+		'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+		'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+		'V', 'W', 'X', 'Y', 'Z'};
+
+	char rot[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+		'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+		'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+		'I', 'J', 'K', 'L', 'M'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				s[i] = rot[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
