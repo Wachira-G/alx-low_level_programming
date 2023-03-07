9bceb88bef70a30177485bd43c84ee71f6dd850f
Gabriel Wachira <gabbyk.com@gmail.com>
2023-03-07 05:52:12 +0300

    Modify 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,19 +7,20 @@
  * in the string accept)
  * @s: string to search through
  * @accept: set of bytes to search for in string - occurence of one
- * return: pointer to position of first match
+ * Return: pointer to position of first match
  * (byte in s that matches one of the bytes in accept)
  */
 
 char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (accept[j] == s[i])
-				return &s[i];
+				return (&s[i]);
 		}
 	}
 	return (0);
