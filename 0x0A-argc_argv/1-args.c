2bed1c36896598233e10918fa4b53196380ce4d7
Gabriel Wachira <gabbyk.com@gmail.com>
2023-03-11 00:41:32 +0300

    Add 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of cli arguments
+ * @argv: list of cli arguments
+ * Return: Always 0;
+ */
+
+int main(int argc, char  __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
