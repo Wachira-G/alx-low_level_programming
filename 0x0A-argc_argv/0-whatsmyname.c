2e1d70abb25bbbc163373cdbf3dfb81fb0a6465d
Gabriel Wachira <gabbyk.com@gmail.com>
2023-03-11 00:32:36 +0300

    Add 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of cli arguments
+ * @argv: list of cli arguments
+ * Return: Always 0;
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
