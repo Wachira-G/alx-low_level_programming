ac5749b3ac53a82dfea56a50f1c1399655462fb5
Gabriel Wachira <gabbyk.com@gmail.com>
2023-03-17 11:03:53 +0300

    Modify 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -9,10 +9,22 @@
  */
 int main(void)
 {
-    char *concat;
+    char *concat, *concat1, *concat2, *concat3;
 
     concat = string_nconcat("Best ", "School !!!", 6);
     printf("%s\n", concat);
     free(concat);
+
+    concat1 = string_nconcat(NULL, "School !!!", 6);
+    printf("%s\n", concat1);
+    free(concat1);
+
+    concat2 = string_nconcat("Best ", NULL, 6);
+    printf("%s\n", concat2);
+    free(concat2);
+
+    concat3 = string_nconcat(NULL, NULL, 6);
+    printf("%s\n", concat3);
+    free(concat3);
     return (0);
 }
--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,30 +16,28 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *result;
-	size_t len1 = 0, len2 = 0, i = 0, j;
+	char *result = NULL;
+	size_t result_len = 0, i = 0, j;
 
 	if (s1 == NULL) /* change to empty strings if null */
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[len1] || s2[len2])/* get s1 and s2 length */
+	while (s1[result_len] || s2[result_len])/* get s1 and s2 length */
 	{
-		len1 = len1 + 1;
-		len2 = len2 + 1;
+		result_len = result_len + 1;
 	}
 
-	result = malloc(len1 + len2 + 1);
+	result = malloc(sizeof(char) * (result_len + 1));
 	if (result == NULL)
 		return (NULL);
 
 	for (i = 0; s1[i] != '\0'; i++)
 		result[i] = s1[i];
-	for (j = 0; s2[j] != '\0' && j < n; j++)
+	for (j = 0; j < n &&  s2[j] != '\0'; j++)
 		result[i + j] = s2[j];
 
 	result[i + j] = '\0';
-
 	return (result);
 }
