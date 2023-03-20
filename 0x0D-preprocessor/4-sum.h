3baebab25674c868f846350af0931700cd251e82
Gabriel Wachira <gabbyk.com@gmail.com>
2023-03-20 16:42:00 +0300

    Add 4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * a function-like macro SUM(x, y)
+ * that computes the sum of the numbers x and y.
+ * @x: first number to sum
+ * @y: second number to sum
+ */
+#define SUM(x, y) (x + y)
+
+#endif
