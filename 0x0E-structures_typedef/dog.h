541d2670daa16a915a815a8ab500ac8229e619b7
Gabriel Wachira <gabbyk.com@gmail.com>
2023-03-20 21:47:01 +0300

    Add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure named dog
+ * @name: character sequence
+ * @age: float
+ * @owner: character sequence
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
