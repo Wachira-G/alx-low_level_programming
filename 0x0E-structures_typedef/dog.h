b455588cb8d7f732a7437f4414401d4e9949bffe
Gabriel Wachira <gabbyk.com@gmail.com>
2023-03-20 23:29:03 +0300

    Modify 1-init_dog.c, dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,9 @@ struct dog
 
 typedef struct dog dog_t;
 
-#include "main.h"
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
