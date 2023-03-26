e774563af7cabdf9c9fd6d6c6053190c7f36ef05
Gabriel Wachira <gabbyk.com@gmail.com>
2023-03-26 20:35:41 +0300

    Update struct description and fix typo in variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,9 +6,14 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct idn - stucture
+ * @id: a character
+ * @rep: a pointer to an array of chars
+ */
 typedef struct idn
 {
 	char id;
-	char * rep;
-} idents;
+	char *rep;
+} identfs;
 #endif
