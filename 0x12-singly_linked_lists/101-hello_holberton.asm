076a4ae930cb2967c94567fa98d7ca44eafe992f
Gabriel Wachira <gabbyk.com@gmail.com>
2023-03-31 00:50:39 +0300

    Modify 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,19 @@
 section .data
     hello db "Hello, Holberton",10
-    len equ $-hello
 
 section .text
-    global _start
+    global main
+    extern printf
 
-    _start:
-        ; print "Hello, Holberton" using printf
-        mov eax, 4          ; system call for write
-        mov ebx, 1          ; file descriptor for stdout
-        mov ecx, hello      ; address of string to write
-        mov edx, len        ; length of string
-        int 0x80            ; call kernel
+main:
+    push rbp
+    mov rbp, rsp
 
-        ; exit program
-        mov eax, 1          ; system call for exit
-        xor ebx, ebx        ; exit status code
-        int 0x80            ; call kernel
+    mov rdi, hello
+    mov rax, 0
+    call printf
+
+    mov eax, 0
+    leave
+    ret
 
