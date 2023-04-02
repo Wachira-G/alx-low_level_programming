2dbbaa88e3ba68c246094452584f03b18a9b4d3b
Gabriel Wachira <gabbyk.com@gmail.com>
2023-04-02 15:19:44 +0300

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -20,7 +20,7 @@ void print_dog(struct dog *d)
 	}
 	if (d->name == NULL)
 	{
-		printf("Name: (nil)\n");
+		printf("Name: \\(nil\\)\n");
 	}
 	else
 	{
@@ -29,7 +29,7 @@ void print_dog(struct dog *d)
 
 	if ((d->age) <= 0.0)
 	{
-		printf("Age: (nil)\n");
+		printf("Age: \\(nil\\)\n");
 	}
 	else
 	{
@@ -38,7 +38,7 @@ void print_dog(struct dog *d)
 
 	if (d->owner == NULL)
 	{
-		printf("Owner: (nil)\n");
+		printf("Owner: \\(nil\\)\n");
 	}
 	else
 	{
