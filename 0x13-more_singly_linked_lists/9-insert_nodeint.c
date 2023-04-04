bc158bf2ae555bff23590ca154a5eb63e1d5b8c8
Gabriel Wachira <gabbyk.com@gmail.com>
2023-04-05 00:36:37 +0300

    Modify 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,7 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 
-	if (*head == NULL || (*head)->next == NULL)
+	if ((*head == NULL || (*head)->next == NULL) && idx == 0)
 	{
 		node_to_push = *head;
 		*head = new_node;
