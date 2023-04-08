c004939aa8517a94cdee0e69b10b3af933c5aa2a
Gabriel Wachira <gabbyk.com@gmail.com>
2023-04-08 22:52:37 +0300

    Add main.h _putchar.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,5 +8,6 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 int get_endianness(void);
+int _putchar(char c);
 
 #endif
