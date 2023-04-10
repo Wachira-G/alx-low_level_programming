21a36415a8834d96316d4099650755560eaad73d
Gabriel Wachira <gabbyk.com@gmail.com>
2023-04-10 12:48:26 +0300

    Remove stdio in 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,10 +1,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
-#include <limits.h>
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
