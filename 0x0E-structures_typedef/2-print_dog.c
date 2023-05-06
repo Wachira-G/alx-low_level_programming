2d372b7374cbc619bf6a68ff5745530f86a18d8d
Gabriel Wachira <gabbyk.com@gmail.com>
2023-05-06 13:56:45 +0300

    Update age=0 handling on 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -27,14 +27,7 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", d->name);
 	}
 
-	if ((d->age) <= 0.0)
-	{
-		printf("Age: %s", "(nil)\n");
-	}
-	else
-	{
-		printf("Age: %f\n", d->age);
-	}
+	printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
 	{
