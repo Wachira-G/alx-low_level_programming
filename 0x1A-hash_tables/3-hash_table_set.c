6f8f3fab00cc8253ef524a0688d4f76572173538
Gabriel Wachira <gabbyk.com@gmail.com>
2023-07-09 22:47:53 +0300

    Fix error in replacing value to existing keys: was not checking existance

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,13 +1,19 @@
 #include "hash_tables.h"
 
+hash_node_t *create_element(const char *key, const char *value);
+
 /**
  * hash_table_set - add an element to the hash table.
  * @ht: the hash table you want ot add or update the key/value to
+ *
  * @key: is the key. cannot be an empty string.
+ *
  * @value: value associated with the key.
- *   value must be duplicated and can ve an empty string.
+ *   value must be duplicated and can be an empty string.
+ *
  * Return: 1 if succeeded or 0 otherwise.
- *   In case of collision, add the new node at the beginning of the list.
+ *
+ * In case of collision, add the new node at the beginning of the list.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
@@ -17,18 +23,65 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || key == NULL || strcmp(key, "") == 0 || ht->size == 0)
 		return (0);
 
-	index = hash_djb2((const unsigned char *) key) % ht->size;
-
-	/*create node for element*/
-	element = malloc(sizeof(hash_node_t));
+	element = create_element(key, value);
 	if (element == NULL)
 		return (0);
+
+	index = hash_djb2((const unsigned char *) key) % ht->size;
+	if (ht->array[index] != NULL) /* ht array at index not empty */
+	{
+		hash_node_t *ptr = ht->array[index];
+
+		while (ptr != NULL) /* traverse the chain checking of key exists */
+		{
+			if (strcmp(ptr->key, key) == 0) /* exits */
+			{
+				free(ptr->value); /*delete current value*/
+				ptr->value = strdup(value);
+
+				free(element->key); /*delete node we intended to insert */
+				free(element->value);
+				free(element);
+
+				if (ptr->value == NULL)
+				{
+					return (0);
+				}
+				return (1);
+			}
+			ptr = ptr->next;
+		}
+		/* does not exist on chain*/
+		element->next = ht->array[index];
+		ht->array[index] = element;
+		return (1);
+	}
+	/* ht array at index was empty (NULL) */
+	element->next = NULL;
+	ht->array[index] = element;
+	return (1);
+}
+
+
+/**
+ * create_element - creates a node(element)
+ * @key: key to the element
+ * @value: the value to the element
+ * Return: the node(element) if successfully created or NULL if not.
+ */
+hash_node_t *create_element(const char *key, const char *value)
+{
+	hash_node_t *element = NULL;
+
+	element = malloc(sizeof(hash_node_t)); /*create node (element) */
+	if (element == NULL)
+		return (NULL);
 
 	element->key = strdup(key);
 	if (element->key == NULL)
 	{
 		free(element);
-		return (0);
+		return (NULL);
 	}
 
 	element->value = strdup(value);
@@ -36,18 +89,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		free(element->key);
 		free(element);
-		return (0);
+		return (NULL);
 	}
 
 	element->next = NULL;
 
-	if (ht->array[index] != NULL)
-	{
-		element->next = ht->array[index];
-		ht->array[index] = element;
-	}
-	else
-		ht->array[index] = element;
-
-	return (1);
+	return (element);
 }
--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -4,15 +4,77 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code
+ * _hash_table_print - prints the hash table key and value
+ * @ht: the hash table
+ * @retval None
+ */
+void _hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *p;
+	char n;
+
+	for (i = 0; ht && i < ht->size; i++)
+	{
+		p = ht->array[i];
+		printf("[%lu]", i);
+		n = 0;
+		while (p)
+		{
+			if (n)
+				printf(", ");
+			printf("'%s': '%s'", p->key, p->value);
+			p = p->next;
+			n = 1;
+		}
+		printf("\n");
+	}
+}
+
+/**
+ * main - check the code for Holberton School students.
  *
  * Return: Always EXIT_SUCCESS.
  */
 int main(void)
 {
 	hash_table_t *ht;
+	int ret;
 
-	ht = hash_table_create(1024);
-	hash_table_set(ht, "betty", "cool");
+	ht = hash_table_create(10);
+	ret = hash_table_set(ht, "Holberton", "is cool");
+	printf("%d\n", ret);
+	_hash_table_print(ht);
+	ret = hash_table_set(ht, "Holberton", "is soooo cooool");
+	printf("%d\n", ret);
+	_hash_table_print(ht);
+	ret = hash_table_set(ht, "Holberton", "is awesome");
+	printf("%d\n", ret);
+	_hash_table_print(ht);
+
+	hash_table_set(ht, "hetairas", "collides with mentioner");
+	hash_table_set(ht, "mentioner", "collides with hetairas");
+
+	hash_table_set(ht, "heliotropes", "collides with neurospora");
+	hash_table_set(ht, "neurospora", "collides with heliotropes");
+
+	hash_table_set(ht, "depravement", "collides with serafins");
+	hash_table_set(ht, "serafins", "collides with depravement");
+
+	hash_table_set(ht, "stylist", "collides with subgenera");
+	hash_table_set(ht, "subgenera", "collides with stylist");
+
+	hash_table_set(ht, "joyful", "collides with synaphea");
+	hash_table_set(ht, "synaphea", "collides with joyful");
+
+	hash_table_set(ht, "redescribed", "collides with urites");
+	hash_table_set(ht, "urites", "collides with redescribed");
+
+	hash_table_set(ht, "dram", "collides with vivency");
+	hash_table_set(ht, "vivency", "collides with dram");
+
+	_hash_table_print(ht);
+	hash_table_delete(ht);
+
 	return (EXIT_SUCCESS);
 }
